5a2c62d1f341cdb52dea54b4a040c624fbbd76e2
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-10 15:32:00 -0800

    Calculating siz of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description: main - prints the size ofthe files to the stdout
+ * Return: returns 0 if succeess
+*/
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
