5704e001dc5e8b9f9894c5d0e481cb6df8445f18
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-10 15:10:14 -0800

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - is the main method that prints to the standard output
+ * Return: 0 when success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
