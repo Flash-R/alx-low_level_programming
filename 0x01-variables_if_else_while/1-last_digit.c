8872f17cbcca1f7f44d0cc9c9f11d22e790b75b2
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-13 14:01:23 -0700

    working with elses and modulus

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /*
   * main - prints out to stdout
-  * Return - always success if the output is zero
+  * Return: - always success if the output is zero
 */
 
 int main(void)
@@ -12,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last_d = n % 10;
 	if (last_d > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
