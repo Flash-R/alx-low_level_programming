c79d1e2d6348822a93eeba60c011a54322878b46
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-13 14:54:07 -0700

    Working and printing both uppercase and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints to thestdout
+ * Return: prints zeron when success
+ *
+*/
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (1 < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
