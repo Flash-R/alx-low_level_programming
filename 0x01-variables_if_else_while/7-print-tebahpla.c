2fdc80622b43824dc166da6aae2deec25f9d5aeb
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-13 15:55:19 -0700

    printing reversed letters

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the reverse of the letters
+ * Return: 0 when success
+*/
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar(10);
+
+	return (0);
+}
