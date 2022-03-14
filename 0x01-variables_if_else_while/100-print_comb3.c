47924fd78af4fd5e3d08814db2325ecee6401b94
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-14 15:44:55 -0700

    working with comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - displays in the stdout
+ * Return: presents 0 if success
+*/
+
+int main(void)
+{
+	int i, j;
+
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
