a2203ce3ee4ea9334730b6899dc8c83c08fafd02
Tumwesigye Ronnie <tumronald1@gmail.com>
2022-03-21 14:39:58 -0700

    working with points to switch values

---
+++ 0x05-pointers_arrays_strings/0-resent_to_98.c
@@ -0,0 +1,11 @@
+include "main.h"
+/**
+ * reset_to_int() - returns the changed value
+ * &*n: is the value for the parameter pointer
+ * Return: reurns zero when success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
