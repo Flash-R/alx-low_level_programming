41b5aed888880577d36362b105300d5ff5b3b863
Flash-R <tumronald1@gmail.com>
2023-03-11 05:04:31 -0800

    Working with commad line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main() - print the file name
+ *return - 1 or 0
+ */
+int main(int argc, char *argv[]){
+	int i;
+	for(i = 0; i < argc; i++){
+		printf("%s\n",argv[i]);
+	}
+	return 0;
+}
