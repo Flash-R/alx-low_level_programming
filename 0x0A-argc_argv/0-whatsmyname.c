8be67370a335047527f6dd8db67ad36ccfbda47b
Flash-R <tumronald1@gmail.com>
2023-03-11 05:08:25 -0800

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- *main() - print the file name
- *return - 1 or 0
+ * main() - print the file name
+ * @argc - number of command line arguments
+ * @argv - vector of the arguments in char* (Strings)
+ * return - 1 or 0
  */
 int main(int argc, char *argv[]){
 	int i;
