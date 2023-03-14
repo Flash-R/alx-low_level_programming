84a69007fc6ae0b658072cce2a3b2147d409298f
Flash-R <tumronald1@gmail.com>
2023-03-14 13:43:19 -0700

    added putchar anf create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <main.h>
+/**
+ * create_array - creates an array of a specfic size
+ * @size - size of the array
+ * @c - character to intiatilaize array
+ * return - pointer to array
+ */
+
+char *create_array(unsigned int size, char c){
+	
+	char *arr;
+	unsigned int i = 0;
+	
+	if(size == 0){
+		return (NULL);
+	}
+	else
+		arr = malloc(size * sizeof(char));
+
+	if(arr == NULL)
+		return (NULL);
+	while(i < size){
+	arr[i] = c;
+	i++;
+	}
+
+	return arr;
+
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write a character on std output
+ * @c: is the character to be printed
+ *
+ * return: 1 when sucess
+ * On error, -1
+ */
+
+void _putchar(char c){
+	return(write(1,%c,1));
+}
