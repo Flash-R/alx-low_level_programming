f79bf27efeabad58a4901b8d47ef31de66fd28a2
Flash-R <tumronald1@gmail.com>
2023-03-14 14:29:14 -0700

    fixwd betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,14 @@
  * create_array - creates an array of a specfic size.
  * @size: size of the array
  * @c: character to intiatilaize array
- * return: pointer to array
+ * Return: pointer to array
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i = 0;
+
 	if (size == 0)
 	{
 		return (NULL);
