68cdde74b4fd4b978ec16621c2ca561391a40990
Flash-R <tumronald1@gmail.com>
2023-03-15 12:59:49 -0700

    String concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib>
+/**
+ * str_concat - concats two strings.
+ * @s1: first string
+ * @s2: second string
+ * Return: the pointer to new longer string.
+ */
+
+char *str_concat(char s1, char s2)
+{
+	char *longstr;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int total = 0;
+	unsigned int i = 0;
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	total = len1 + len2;
+	longstr = malloc(total * sizeof(*longstr));
+	while (i < len1)
+	{
+		longstr[i] = s1[i];
+		i++;
+	}
+	unsigned int j = 0;
+	while (j < len2)
+	{
+		longstr[i] = s2[j];
+		j++;
+		i++;	
+	}
+	return (longstr);
+}
