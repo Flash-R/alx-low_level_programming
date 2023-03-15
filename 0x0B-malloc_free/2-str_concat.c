92eec59f239ef6271e94029489d697c5374f3358
Flash-R <tumronald1@gmail.com>
2023-03-15 13:29:32 -0700

    String concat bugs fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,7 @@ char *str_concat(char *s1, char *s2)
 	char *longstr;
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
+	unsigned int j = 0;
 	unsigned int total = 0;
 	unsigned int i = 0;
 
@@ -30,7 +31,6 @@ char *str_concat(char *s1, char *s2)
 		longstr[i] = s1[i];
 		i++;
 	}
-	unsigned int j = 0;
 	while (j < len2)
 	{
 		longstr[i] = s2[j];
