5fefd54f1e67deffa4befa3eb9c5387728cc9854
Flash-R <tumronald1@gmail.com>
2023-03-17 02:08:57 -0700

    added the mallock checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 /**
  * malloc_checked - returns the pointer
  * @b: size
- * return: address or 98;
+ * Return: address or 98;
  */
 void *malloc_checked(unsigned int b)
 {
@@ -10,7 +10,8 @@ void *malloc_checked(unsigned int b)
 	s = malloc(b);
 	if (s == NULL)
 	{
-	exit(98);
+		exit(98);
 	}
-	return (s);
+	else
+		return (s);
 }
