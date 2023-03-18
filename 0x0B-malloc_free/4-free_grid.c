717cbd7975783635de637a627a809bdee12ea1df
Flash-R <tumronald1@gmail.com>
2023-03-18 12:24:33 -0700

    free the grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the memory previously allocated to a 2D array
+ * @array: the grid pointer
+ * @height: height of grid
+ * Return: 0;
+ */
+
+void free_grid(int **grid, int height)
+{
+
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
