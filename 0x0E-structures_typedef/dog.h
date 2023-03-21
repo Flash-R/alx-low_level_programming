b5959f512810ba22a7675936aa19be6da4f8e3f4
Flash-R <tumronald1@gmail.com>
2023-03-21 12:42:31 -0700

    Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef Dog_H
+#define Dog_H
+
+/**
+ * struct dog - dog structure
+ * @age: age of the dog
+ * @name: name of dog
+ * @owner: owner of dog
+ */
+typedef struct Dog
+{
+	char *name;
+	int age;
+	char *owner;
+} dog_1
