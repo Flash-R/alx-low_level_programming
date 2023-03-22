e3213c0a4a38c64520ab74fb8b650822e96b09db
Flash-R <tumronald1@gmail.com>
2023-03-21 18:06:36 -0700

    free the dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * fre_dog - frees the dog memory
+ * @d: the objet of dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
