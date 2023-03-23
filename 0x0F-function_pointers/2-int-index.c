d2e0624b30305e30e8071ea6ffeaa3d8f15dc5e0
Flash-R <tumronald1@gmail.com>
2023-03-23 15:18:41 -0700

    compare

---
+++ 0x0F-function_pointers/2-int-index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that compares two ints
+ * @array: array od elements
+ * @size: number of items
+ * @cmp: number of items
+ * Return: -1 if fail or an index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (array == NULL || cmp == NULL || size <= 0)
+		return -1;
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return i;
+	}
+	return -1;
+}
