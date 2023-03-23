235aece2bea58011c1117338072da307508103d9
Flash-R <tumronald1@gmail.com>
2023-03-23 06:28:22 -0700

    print v1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
+#include <stdlib.h>
 #include "funtion_pointers.h"
 
 /**
  * print_name - prints the name
  * @name: the name to be printed
  * @f: the function that prints the name
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != 0 || f != 0)
 		f(name);
 }
