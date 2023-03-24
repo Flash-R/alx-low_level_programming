68860d41a0744d025d8e662c1c5891ef8cd2770e
Flash-R <tumronald1@gmail.com>
2023-03-24 02:01:39 -0700

    files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function to add
+ * @a: first argument
+ * @b: second argument
+ * Return: The addition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function to substract
+ * @a: First argument
+ * @b: Second argument
+ * Return: subtracted value
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function to multiply
+ * @a: First argument
+ * @b: Second argument
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division
+ * @a: First aargument
+ * @b: Second argument
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modules
+ * @a: First argument
+ * @b: Second argument
+ * Return: Modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
