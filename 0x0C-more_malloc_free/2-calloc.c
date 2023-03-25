4dd6408e74cc0c6eaf0240eb78cd9fbaa981fb5d
Flash-R <tumronald1@gmail.com>
2023-03-25 01:51:31 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,25 @@
 #include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 
 /**
  * _calloc - allocates memory to array
@@ -9,12 +30,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb *size);
 	if (p ==NULL)
 		return (NULL);
+	_memset(p, 0, size * nmemb);
 	return (p);
 }
