29446f9e4a8a6a9d61e1361419055429811b33e8
Flash-R <tumronald1@gmail.com>
2023-04-04 15:13:53 -0700

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - add and integer node at the start
+ * @head: head
+ * @n: data
+ * Return: address of new longer list
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t node;
+
+	node = malloc(sizeof(node));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = *head;
+	*head = node;
+
+	return (*head);
+}
